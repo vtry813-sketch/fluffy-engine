3a0db34c0248c303306169fa9022f410cb32c484
vtry813-sketch <vtry813@gmail.com>
2025-12-12 16:01:30 -0300

    Update and rename inconnu/inconnu.js to inconnu.js

--- inconnu.js
+++ inconnu.js
@@ -3,16 +3,16 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('../index'); 
+let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/inconnu/pair.html')
+    res.sendFile(__path + '/pair.html')
 });
 app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/inconnu/main.html')
+    res.sendFile(__path + '/main.html')
 });
 
 app.use(bodyParser.json());
@@ -20,7 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`
-ᴍɪɴɪ ɪɴᴄᴏɴɴᴜ xᴅ ᴠ²  
+Don't Forget To Give Star 🌟🌟🌟🌟
+
 
 Server running on http://localhost:` + PORT)
 });
