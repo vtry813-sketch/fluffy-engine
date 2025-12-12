1e716377e9b5f804c70418e419431da57e330357
vtry813-sketch <vtry813@gmail.com>
2025-12-12 19:46:38 -0300

    Update inconnu.js

--- inconnu.js
+++ inconnu.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+__path = process.cwd()
 const bodyParser = require("body-parser");
-
-__path = process.cwd();
 const PORT = process.env.PORT || 8000;
-
-// ⚠️ Ton fichier index.js n'est PAS un middleware Express.
-// On charge simplement le bot, sans l'ajouter à app.use()
-require('./index');
+let code = require('./pair'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-// Pages fixes
-app.get('/pair', (req, res) => {
-    res.sendFile(path.join(__path, 'pair.html'));
+app.use('/code', code);
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html')
+});
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html')
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__path, 'main.html'));
-});
-
-// Middleware BodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,8 +22,9 @@ app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star 🌟🌟🌟🌟
 
-Server running on http://localhost:${PORT}
-`);
+
+Server running on http://localhost:` + PORT)
 });
 
 module.exports = app;
+
