e17824d18d37c3501f61cab12a76c9cf9703f4e2
vtry813-sketch <vtry813@gmail.com>
2025-12-12 16:40:04 -0300

    Update inconnu.js

--- inconnu.js
+++ inconnu.js
@@ -3,12 +3,12 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
+let code = require('./index'); 
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
-app.use('/index', async (req, res, next) => {
+app.use('/pair', async (req, res, next) => {
     res.sendFile(__path + '/pair.html')
 });
 app.use('/', async (req, res, next) => {
