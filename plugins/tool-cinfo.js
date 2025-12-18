8d7fcc1b6f0b5461901b963b943729f737618e5e
vtry813-sketch <vtry813@gmail.com>
2025-12-18 10:59:34 -0300

    INCONNU BOY TECH 🍂

--- plugins/tool-cinfo.js
+++ plugins/tool-cinfo.js
@@ -5,7 +5,7 @@ cmd({
     pattern: "countryinfo",
     alias: ["cinfo", "country","cinfo2"],
     desc: "Get information about a country",
-    category: "info",
+    category: "tool",
     react: "🌍",
     filename: __filename
 },
